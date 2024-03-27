a14e080563a868bf1e5ab02e7fff5c4da710e3d1
yleal <63595588+yleal@users.noreply.github.com>
2024-03-27 19:31:08 -0400

    Cambio en "Conserva tu número"
    Eliminar una clase de sobra

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -10,7 +10,7 @@ type StepProps = {
 const Step: React.FC<StepProps> = ({step, children, style, className}) => {
   return (
     <div
-      className={`sm:col-span-2 p-3 md:p-4 lg:p-5 xl:p-6 ml-3 md:ml-6 lg:ml-9 xl:ml-12 flex items-center rounded-tl-full rounded-bl-full font-medium mb-3 sm:mb-4 md:mb-5 lg:mb-6 xl:mb-7 ${className || ''}`}
+      className={`p-3 md:p-4 lg:p-5 xl:p-6 ml-3 md:ml-6 lg:ml-9 xl:ml-12 flex items-center rounded-tl-full rounded-bl-full font-medium mb-3 sm:mb-4 md:mb-5 lg:mb-6 xl:mb-7 ${className || ''}`}
       style={style || {}}
     >
       <div
