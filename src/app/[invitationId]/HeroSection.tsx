5d710beb2c35cd4356fb0779cabae2a31e320298
Jonathan Mangarret <jmangarret@gmail.com>
2024-04-03 23:38:41 -0400

    fix section video

--- src/app/[invitationId]/HeroSection.tsx
+++ src/app/[invitationId]/HeroSection.tsx
@@ -18,12 +18,12 @@ export default function HeroSection() {
       <div className="sm:flex">
         <div className="w-full px-3 md:px-5 lg:px-10 my-10 sm:my-20 md:my-20 lg:my-20 mx-5 xl:mx-24">
           <p className="text-lg sm:text-xl md:text-xl lg:text-xl xl:text-xl text-center">
-          ¿Aun tienes dudas? <span className="font-medium ">Dale clic al siguiente botón</span> y descubre más información.
+          ¿Aún tienes dudas? <span className="font-medium ">Dale clic al siguiente botón</span> y descubre más información.
           </p>
         </div>
       </div>
       <div className="col-span-12 md:col-span-4 mb-10 sm:mb-20 md:mb-30 lg:mb-40">
-        <div className="button-container text-center lg:w-72 mx-auto mb-30">
+        <div className="button-container text-center lg:w-72 mx-auto mb-30 flex justify-center">
           <button className={`btn-width multi-border font-medium block`}>
             CONOCE MÁS
           </button>
